8f32e9506fbf546f1436369a66066def7d42bca8
LiWeiqiang <1072864729@qq.com>
2023-02-25 13:05:30 +0800

    'update'

---
+++ lia-system/src/main/java/com/lia/system/result/exception/ErrorController.java
@@ -0,0 +1,40 @@
+package com.lia.system.result.exception;
+
+import com.lia.system.result.SysResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("${server.error.path:/error}")
+public class ErrorController extends BasicErrorController {
+
+
+    @Value("${server.error.path:/error}")
+    private String path;
+
+
+    public ErrorController(ServerProperties serverProperties) {
+        super(new DefaultErrorAttributes(), serverProperties.getError());
+    }
+
+
+    /**
+     * 404无法被全局异常处理器捕获，在此处特殊处理
+     */
+    @Override
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        HttpStatus status = getStatus(request);
+        throw new HttpException(SysResult.valueOf(status.value()));
+    }
+
+}
